bb878f8ee1465e99a70d9fdee16575a2db489ffa
hssullivan <david.melchor.bluetrain@gmail.com>
2024-11-20 23:58:20 +0100

    probando output

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 const isProd = process.env.NODE_ENV === 'production';
 
 module.exports = {
+  output: 'export',
   basePath: isProd ? '/portfolio' : '',  // Ajusta la ruta base si es necesario
   assetPrefix: isProd ? '/portfolio/' : '',  // También ajusta el prefijo de los assets
   images: {
