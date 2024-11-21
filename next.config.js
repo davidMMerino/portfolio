fd69c4513550238bd953c6e795beb90183e5cbf4
hssullivan <david.melchor.bluetrain@gmail.com>
2024-11-21 23:58:47 +0100

    next.config without bashPath

--- next.config.js
+++ next.config.js
@@ -1,10 +1,7 @@
 const isProd = process.env.NODE_ENV === 'production';
 
 module.exports = {
-  output: 'export',
-  basePath: isProd ? '/portfolio' : '',  // Ajusta la ruta base si es necesario
-  assetPrefix: isProd ? '/portfolio/' : '',  // También ajusta el prefijo de los assets
   images: {
-    unoptimized: true, // Desactiva la optimización de imágenes si es necesario
+    unoptimized: true,
   },
 };
